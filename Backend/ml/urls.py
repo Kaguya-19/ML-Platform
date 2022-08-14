bbc51b7fe5a89864f732ead1dfb3e4913aaf6259
PENGUIN09-28 <1624396822@qq.com>
2022-08-14 19:59:45 +0800

    等待返回初版

--- Backend/ml/urls.py
+++ Backend/ml/urls.py
@@ -6,5 +6,7 @@ app_name = 'ml'
 urlpatterns = [
     path('model/add', views.model_add),
     path('model/<int:model_id>', views.model_info),
-    path('test/add',views.test_file_add),
+    path('model/delete/<int:model_id>', views.model_delete),
+    path('test/add', views.test_file_add), # 测试文件添加
+    path('test/task', views.new_task) # 选择好模型开始测试
 ]

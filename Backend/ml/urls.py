acbe20a5c59947f29809060bffdc915b73669a05
yq <2562722715@qq.com>
2022-08-16 22:52:31 +0800

    增加test的rest api

--- Backend/ml/urls.py
+++ Backend/ml/urls.py
@@ -6,6 +6,7 @@ app_name = 'ml'
 urlpatterns = [
     path('model', views.model_api),
     path('model/<int:model_id>', views.model_info_api),
-    path('test/add',views.test_file_add),
+    path('test',views.test_api),
+    path('test/<int:tested_file_id>',views.test_info_api),
     path('test/task', views.new_task) # 选择好模型开始测试
 ]

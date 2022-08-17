7884820373078e948e2cbd5af900686301a1349e
yq <2562722715@qq.com>
2022-08-17 20:48:44 +0800

    增加Service_info模型和对应api函数，修改了Test_info

--- Backend/ml/urls.py
+++ Backend/ml/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('model/<int:model_id>', views.model_info_api),
     path('test',views.test_api),
     path('test/<int:tested_file_id>',views.test_info_api),
-    path('test/task', views.new_task) # 选择好模型开始测试
+    path('test/task', views.new_task), # 选择好模型开始测试
+    # 部署
+    path('service',views.service_api),
+    path('service/<int:service_id>',views.service_info_api),
 ]
